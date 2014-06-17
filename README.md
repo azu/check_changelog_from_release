7393111ec760baa1af24db47aabf02a64df256dc
azu <azuciao@gmail.com>
2014-06-18 00:41:18 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# check_changelog_from_release
+
+![img](http://take.ms/qtPJs)
+
+## Installation
+
+``` sh
+npm install
+npm run build > check_changelog_from_release.user.js
+```
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name        CheckChangelogFromGithubRelease
+// @namespace   http://efcl.info/
+// @description Check ChangeLog
+// @license     MIT
+// @include     https://github.com/*/*/releases/tag/*
+// @version     1
+// ==/UserScript==
+"use strict";
+var githubAPI = require("./lib/github_api");
+var githubDOM = require("./lib/github_dom");
+var searcher = require("./lib/seacher");
+var injector = require("./lib/injector");
+var treePromise = new Promise(function (resolve, reject) {
+    githubAPI.getTree({
+        "owner": githubDOM.getOwner(),
+        "name": githubDOM.getRepoName(),
+        "sha": githubDOM.getSha()
+    }, function (error, res) {
+        if (error) {
+            reject(error);
+        } else {
+            resolve(res);
+        }
+    })
+});
+
+function getFileRawPromisefy(URL) {
+    return new Promise(function (resolve, reject) {
+        githubAPI.getFileRawContent(URL, function (error, result) {
+            if (error) {
+                reject(error);
+            } else {
+                console.log(result);
+                resolve(result);
+            }
+        });
+    });
+}
+treePromise.then(JSON.parse)
+    .then(searcher.findChangeLogInTrees)
+    .then(function (result) {
+        if (!result) {
+            return Promsie.reject(new Error("Not found CHANGELOG in Repogitory."));
+        }
+        injector.injectChangelogLink(result.path);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+
