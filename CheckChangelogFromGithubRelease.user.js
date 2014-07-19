5d066bfa70accf04af9131474a53a1001a644b4c
azu <azuciao@gmail.com>
2014-07-20 02:42:13 +0900

    Release 0.0.5

--- CheckChangelogFromGithubRelease.user.js
+++ CheckChangelogFromGithubRelease.user.js
@@ -101,8 +101,7 @@ function getTagName() {
     return location.pathname.split("/").pop();
 }
 function getRepoName() {
-    var repo = commandBar.dataset.repo.split("/").pop()
-    return repo;
+    return commandBar.dataset.repo.split("/").pop();
 }
 function getRepo() {
     return commandBar.dataset.repo;
@@ -124,15 +123,20 @@ module.exports = {
  */
 "use strict";
 var releaseMeta = document.querySelector(".release-meta");
+var tagRef = document.querySelector(".tag-references");
 var githubDOM = require("./github_dom");
 const GITHUBDOMAIN = "https://github.com/";
 function injectChangelogLink(filePath) {
     var span = document.createElement("span");
     var a = document.createElement("a");
-    a.href = GITHUBDOMAIN + githubDOM.getRepo() + "/blob/"+ githubDOM.getTagName() + "/" + filePath;
+    a.href = GITHUBDOMAIN + githubDOM.getRepo() + "/blob/" + githubDOM.getTagName() + "/" + filePath;
     a.textContent = "CHANGELOG FILE";
     span.appendChild(a);
-    releaseMeta.appendChild(span);
+    if (tagRef.nextSibling === null) {
+        tagRef.parentNode.appendChild(span); // 末尾に追加
+    } else {
+        tagRef.parentNode.insertBefore(span, tagRef.nextSibling); // 基準ノードの後ろに追加
+    }
 }
 
 module.exports = {
